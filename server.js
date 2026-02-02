c04759374883e293217bd8eaf85e19442db4a590
Solomind <solomind@Solo.local>
2026-02-03 06:16:30 +0800

    Fix: use PORT 8080 as default

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 const dbPath = path.join(__dirname, 'data/geothermal.db');
 
 let db = null;
